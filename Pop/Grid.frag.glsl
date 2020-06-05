f6f872ce268099256222c38ca48286ba56eb7f1c
Graham Reeves <graham@grahamreeves.com>
2020-06-05 16:54:13 +0100

    Fix for webgl

--- Pop/Grid.frag.glsl
+++ Pop/Grid.frag.glsl
@@ -27,7 +27,14 @@ float3 GetNumberColour(int Number)
 	Colours[7] = float3(f,0,0);
 	Colours[8] = float3(f,f,0);
 
-	return Colours[ Number % 10 ];
+	//return Colours[ Number % 10 ];
+	//if ( Number > 10 )
+	//	Number -= 10;
+	//return Colours[ Number ];
+	for ( int i=0;	i<10;	i++ )
+		if ( Number == i )
+			return Colours[i];
+	return Colours[0];
 }
 
 void GetGridValue(out int NeighbourCount,out bool IsMine,out bool IsHidden)
@@ -48,25 +55,25 @@ float3 GetNormalYellowGreenBlue(float Normal)
 {
 	if ( Normal < 0.333 )
 	{
-		Normal = Range( 0, 0.333, Normal );
-		return float3( 1-Normal, 1, 0 );
+		Normal = Range( 0.0, 0.333, Normal );
+		return float3( 1.0-Normal, 1.0, 0.0 );
 	}
 	else if ( Normal < 0.666 )
 	{
 		Normal = Range( 0.333, 0.666, Normal );
-		return float3( 0, 1, Normal );
+		return float3( 0.0, 1.0, Normal );
 	}
 	else
 	{
 		Normal = Range( 0.666, 1.0, Normal );
-		return float3( 0, 1-Normal, 1 );
+		return float3( 0.0, 1.0-Normal, 1.0 );
 	}
 }
 
 float2 GetFontUv(int Number,float2 LocalUv)
 {
 	float u = LocalUv.x;
-	float v = (Number+LocalUv.y) / float(FONT_CHAR_COUNT);
+	float v = (float(Number)+LocalUv.y) / float(FONT_CHAR_COUNT);
 	return float2(u,v);
 }
 
@@ -99,8 +106,8 @@ float3 GetNumberColour(int Number,bool Hidden,float2 LocalUv)
 		//	return LightGrey;
 	}
 	
-	LocalUv.x = mix( -Border, 1+Border, LocalUv.x);
-	LocalUv.y = mix( -Border, 1+Border, LocalUv.y);
+	LocalUv.x = mix( -Border, 1.0+Border, LocalUv.x);
+	LocalUv.y = mix( -Border, 1.0+Border, LocalUv.y);
 	/*
 	if ( LocalUv.x < -Border/2.0 || LocalUv.y < -Border/2.0 )
 		return HighOut;
@@ -112,8 +119,8 @@ float3 GetNumberColour(int Number,bool Hidden,float2 LocalUv)
 	if ( max(LocalUv.x,LocalUv.y) > 1.0 )
 		return LowIn;
 
-	LocalUv.x = mix( -InnerBorder, 1+InnerBorder, LocalUv.x);
-	LocalUv.y = mix( -InnerBorder, 1+InnerBorder, LocalUv.y);
+	LocalUv.x = mix( -InnerBorder, 1.0+InnerBorder, LocalUv.x);
+	LocalUv.y = mix( -InnerBorder, 1.0+InnerBorder, LocalUv.y);
 	if ( LocalUv.x < 0.0 || LocalUv.y < 0.0 )
 		return Inside;
 	if ( LocalUv.x > 1.0 || LocalUv.y > 1.0 )
@@ -143,9 +150,9 @@ void main()
 	float2 xy = uv * GridSize;
 	float2 LocalUv = fract( xy );
 
-	gl_FragColor = float4(LocalUv,0,1);
+	gl_FragColor = float4(LocalUv,0.0,1.0);
 	gl_FragColor.xyz = GetNumberColour(NeighbourCount,IsHidden,LocalUv);
-	gl_FragColor.w = 1;
+	gl_FragColor.w = 1.0;
 	/*
 	float4 Sample = texture2D( Texture, uv );
 	Sample.x = Sample.x != 0 ? 1 : 0;
